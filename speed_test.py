6ff1daafe74b263b5e0d997cf53cdcfdb67eef71
dronamadhuri <dadidronamadhuri@gmail.com>
2026-03-06 23:29:57 +0530

    Initial commit - Internet Speed Test App

---
+++ speed_test.py
@@ -0,0 +1,13 @@
+import speedtest
+
+print("Checking Internet Speed...")
+
+st = speedtest.Speedtest()
+
+download_speed = st.download() / 1_000_000
+upload_speed = st.upload() / 1_000_000
+ping = st.results.ping
+
+print("Download Speed:", round(download_speed,2), "Mbps")
+print("Upload Speed:", round(upload_speed,2), "Mbps")
+print("Ping:", round(ping,2), "ms")
